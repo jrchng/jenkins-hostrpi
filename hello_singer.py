fb74b458395928acf78ae8e70df5611627198e16
jingrouchng <JingRou.Chng@latticesemi.com>
2024-09-22 18:41:57 +0800

    correct singer2 name

--- hello_singer.py
+++ hello_singer.py
@@ -3,5 +3,5 @@ singer = "Adele"
 print(f"{singer} sang {songname}")
 
 songname2 = "Do you wanna build a snowman"
-singer2 = "Christine"
+singer2 = "Kristen Bell"
 print(f"{singer2} sang {songname2}")
