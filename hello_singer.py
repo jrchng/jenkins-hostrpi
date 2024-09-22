814f58d73b663f4c279eb834b96b87f6c6435931
jingrouchng <JingRou.Chng@latticesemi.com>
2024-09-22 16:39:10 +0800

    add singer2

--- hello_singer.py
+++ hello_singer.py
@@ -2,3 +2,6 @@ songname="Hello from the other side"
 singer = "Adele"
 print(f"{singer} sang {songname}")
 
+songname2 = "Do you wanna build a snowman"
+singer2 = "Christine"
+print(f"{singer2} sang {songname2}")
