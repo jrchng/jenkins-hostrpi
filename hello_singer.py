c5417c46c7b127f660818ea06ac521df029d9367
jingrouchng <JingRou.Chng@latticesemi.com>
2024-10-18 16:51:21 +0800

    jenkins host rpi successful

--- hello_singer.py
+++
@@ -1,7 +0,0 @@
-songname="Hello from the other side"
-singer = "Adele"
-print(f"{singer} sang {songname}")
-
-songname2 = "Do you wanna build a snowman"
-singer2 = "Kristen Bell"
-print(f"{singer2} sang {songname2}")
