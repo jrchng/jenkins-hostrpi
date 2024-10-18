95e2b6c8abbaca6711f99e95ad713176e810beba
jingrouchng <JingRou.Chng@latticesemi.com>
2024-10-18 16:50:56 +0800

    241018 test on teton: motor send hardreset cmd successful

---
+++ run_motor_hardreset.py
@@ -0,0 +1,32 @@
+import asyncio
+import websockets
+import signal
+
+async def websocket_client(uri):
+    async with websockets.connect(uri) as websocket:
+        print("Connected to the server.")
+
+        # Send an initial message to the server
+        await websocket.send("hardreset 30")
+        print("Requesting Motorcontroller to hardreset")
+
+        # Continuously receive messages from the server
+        try:
+            while True:
+                message = await websocket.recv()
+                print(f"Received message: {message}")
+                # exit condition: check for specific message
+                if message == "SUCCESSFUL: Wakepc completed" :
+                    print("Successful wakepc. Closing connection.")
+                    break
+        except websockets.ConnectionClosed:
+            print("Connection closed by the server.")
+        finally:
+            await websocket.close()
+            print("Websocket connection closed.")
+
+# Replace 'ws://example.com/socket' with your WebSocket server URI
+uri = 'ws://192.168.0.35:8000'
+
+# Run the WebSocket client
+asyncio.run(websocket_client(uri))
